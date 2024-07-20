f144c3e635636ab7985a191728c038af8d4f6adf
sominola <sominola@gmail.com>
2024-07-20 20:45:40 +0300

    fix insta reels

--- TeleBot.AwsLambdaAOT/Handlers/TextHandlers/InstaReelsHandler.cs
+++ TeleBot.AwsLambdaAOT/Handlers/TextHandlers/InstaReelsHandler.cs
@@ -2,6 +2,7 @@
 using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MimeTypes;
@@ -12,7 +13,7 @@ using TeleBot.Lib.Models;
 
 namespace TeleBot.AwsLambdaAOT.Handlers.TextHandlers;
 
-public class InstaReelsHandler(
+public partial class InstaReelsHandler(
     IHttpClientFactory httpClientFactory,
     ILogger logger,
     IOptions<AppOptions> options
@@ -29,16 +30,17 @@ public class InstaReelsHandler(
         if (response.IsSuccessStatusCode)
         {
             var instaResponse = await response.Content
-                .ReadFromJsonAsync(LambdaJsonContext.Default.FastDlResponse, ct);
+                .ReadFromJsonAsync(LambdaJsonContext.Default.InstsaResponse, ct);
 
             if (instaResponse is null)
                 throw new Exception("InstaResponse is null, return");
 
-            var (url, isVideo) = GetUrl(instaResponse);
+            var url = GetUrl(instaResponse);
             logger.LogInformation("URL: {Url}", url);
 
             using var contentResponse = await _defaultHttpClient.GetAsync(url, ct);
             var extension = MimeTypeMap.GetExtension(contentResponse.Content.Headers.ContentType!.MediaType);
+            var isVideo = extension.Contains("mp4");
             await using var stream = await contentResponse.Content.ReadAsStreamAsync(ct);
 
             if (isVideo)
@@ -67,47 +69,49 @@ public class InstaReelsHandler(
         }
     }
 
-    private static (string url, bool isVideo) GetUrl(FastDlResponse response)
+    private static string GetUrl(InstsaResponse response)
     {
-        var isVideo = response.Url.Any(x => x.Type == "mp4");
-        if (isVideo)
-        {
-            var videoObj = response.Url.FirstOrDefault(x => x is { Type: "mp4", Quality: 1080 } || x.Type == "mp4");
-            return (videoObj!.Url, true);
-        }
+        var match = MyRegex().Match(response.Data);
 
-        var photoObj = response.Url.FirstOrDefault(x => x.Type is "heic" or "jpg");
-        if (photoObj is null)
-            throw new Exception($"{nameof(photoObj)} is null");
+        if (!match.Success) throw new Exception("Cannot get insta url");
+        var url = match.Groups[1].Value.Replace("amp;", "");
+        return url;
 
-        return (photoObj.Url, false);
     }
 
     private HttpRequestMessage BuildMessage(string url)
     {
-        var body = new FastDlRequest
+        var dic = new Dictionary<string, string>()
         {
-            Url = url,
-            Ts = 1719053861511,
-            _Ts = 1718869499625,
-            _Tsc = 784590,
-            _S = "e75cf6ed60570c6bd189433a513e5a9c770c169cd48c52ac6df800a138857fea"
+            { "q", url },
+            { "t", "media" },
+            { "lang", "ru" },
         };
 
-        var serialized = JsonSerializer.Serialize(body, LambdaJsonContext.Default.FastDlRequest);
         var req = new HttpRequestMessage(HttpMethod.Post, options.Value.InstagramApiUrl)
         {
-            Content = new StringContent(serialized, Encoding.UTF8, MediaTypeNames.Application.Json)
+            Content = new FormUrlEncodedContent(dic)
         };
 
-        req.Headers.Add("Accept", "*/*");
-        req.Headers.Add("Accept-Language", "en-US,en;q=0.5");
+        req.Headers.Add("Accept", "application/json, text/plain, */*");
+        req.Headers.Add("Accept-Encoding", "gzip, deflate, br, zstd");
+        req.Headers.Add("Accept-Language", "uk-UA,uk;q=0.9,ru-UA;q=0.8,ru;q=0.7,en-US;q=0.6,en;q=0.5");
+        req.Headers.Add("Cookie", "uid=a8d272e5c2c41435; adsAfterSearch=67; adsPopupClick=79; adsForm=30");
+        req.Headers.Add("Origin", "https://saveig.app");
+        req.Headers.Add("Referer", "https://saveig.app/");
+        req.Headers.Add("Priority", "nu=1, i");
+        req.Headers.Add("Sec-Ch-Ua", "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"");
+        req.Headers.Add("Sec-Ch-Ua-Platform", "\"Windows\"");
         req.Headers.Add("Sec-Fetch-Dest", "empty");
         req.Headers.Add("Sec-Fetch-Mode", "cors");
         req.Headers.Add("Sec-Fetch-Site", "same-origin");
         req.Headers.Add("User-Agent",
-            "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36");
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");
+
 
         return req;
     }
+
+    [GeneratedRegex("href=\"(https://[^\"]+)\"")]
+    private static partial Regex MyRegex();
 }
--- TeleBot.AwsLambdaAOT/Responses/InstaResponse.cs
+++ TeleBot.AwsLambdaAOT/Responses/InstaResponse.cs
@@ -2,46 +2,9 @@
 
 namespace TeleBot.AwsLambdaAOT.Responses;
 
-public class FastDlRequest
+
+public class InstsaResponse
 {
-    [JsonPropertyName("url")]
-    public string Url { get; set; } = null!;
-
-    [JsonPropertyName("ts")]
-    public long Ts { get; set; }
-
-    [JsonPropertyName("_ts")]
-    public long _Ts { get; set; }
-
-    [JsonPropertyName("_tsc")]
-    public long _Tsc { get; set; }
-    
-    [JsonPropertyName("_s")]
-    public string _S { get; set; } = null!;
-}
-
-public class FastDlResponse
-{
-    [JsonPropertyName("url")]
-    public FastDlUrlResponse[] Url { get; set; } = null!;
-
-   
-}
-
-public class FastDlUrlResponse
-{
-    [JsonPropertyName("url")]
-    public string Url { get; set; } = null!;
-    
-    [JsonPropertyName("name")]
-    public string Name { get; set; } = null!;
-    
-    [JsonPropertyName("type")]
-    public string Type { get; set; } = null!;
-    
-    [JsonPropertyName("ext")]
-    public string Ext { get; set; } = null!;
-    
-    [JsonPropertyName("quality")]
-    public long? Quality { get; set; }
+    [JsonPropertyName("data")]
+    public string Data { get; set; } = null!;
 }
